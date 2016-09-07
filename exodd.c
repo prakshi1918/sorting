6c9ede589d2be9159fae6dad09b3f1f2c883fe1f
prakshi1918 <prakshijain198@gmail.com>
2016-09-07 10:51:06 +0530

    Add files via upload

---
+++ exodd.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int *a,n,i=0,nm=0;
+    printf("enter the limit");
+    scanf("%d",&n);
+    a=(int *)malloc(sizeof(int)*n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    nm=a[0];
+    for(i=0;i<n-1;i++)
+    {
+        nm=nm^a[i+1];
+        printf("odd no of time occuring  no is %d",nm);
+    return 0;
+    }
+}
